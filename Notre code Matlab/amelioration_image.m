d11031393808c171842df7a25825c1e658459430
Nassime631 <nassimejamadi@gmail.com>
2020-05-06 10:49:49 +0200

    code image_processing

---
+++ Notre code Matlab/amelioration_image.m
@@ -0,0 +1,32 @@
+clear all 
+close all
+[im,map]=imread('nutella.PNG');
+size(im);
+size(map);
+subplot(2,2,1);
+image(im);
+ndg=0.3*im(:,:,1)+0.59*im(:,:,2)+0.11*im(:,:,3);
+subplot(2,2,2);
+image(ndg);
+mapndg=([0:255]/255)'*[1 1 1];
+colormap(mapndg);
+
+%convolution(application d'un filtre)
+NL=size(ndg,1);
+NC=size(ndg,2);
+ndg=double(ndg);
+masque=[-1 -1 -1;-1 9 -1;-1 -1 -1];
+imconv=zeros(NL,NC);
+W=1;
+offset=0;
+for L=2:NL-1,
+    for C=2:NC-1,
+        imconv(L,C)=offset+sum(sum(masque.*ndg(L-1:L+1,C-1:C+1)))/W;
+    end
+end
+subplot(2,2,3);
+image(imconv)
+colormap(mapndg)
+
+
+
