b7f44c70201ce27b46488dc0a06fbf50eaaee3e1
Gaël <61406755+mekacraft@users.noreply.github.com>
2020-05-30 15:40:23 +0200

    photo rapport

--- Notre code Matlab/Lecture de code-barres avec affichage/isSameOrientation.m
+++ Notre code Matlab/Lecture de code-barres avec affichage/isSameOrientation.m
@@ -3,7 +3,7 @@ function orientationIdentique = isSameOrientation(o1,o2)
     o1=o1+90;
     o2=o2+90;
     orientationIdentique=0;
-    if abs(o1-o2)<10 || abs(o1-o2)>170
+    if abs(o1-o2)<5 || abs(o1-o2)>175
        orientationIdentique=1;
     end
 end
